a83568547bbe305564085ca9e0c4ab80f2a98ed7
pcnay <pcnay2003@gmail.com>
2018-06-12 18:29:32 -0700

    Se define el metodo "Read" de la tabla "Status"

--- MVC02/StatusModel.php
+++ MVC02/StatusModel.php
@@ -28,7 +28,31 @@
     // Si no tiene valor el parametro pasado, se le asigna en blanco.
     public function read($status_id = '')
     {
-    
+      // Se utilizara un operador ternario para asignar la consulta.
+      // Se agrega la doble comilla, ya que se esta validando con la variable $status_id
+      $sql = ($status_id != '')
+      ?"SELECT * FROM status WHERE status_id = $status_id"
+      :"SELECT * FROM status";
+
+      /* 
+      Es lo mismo del anterior
+      if ($status_id != '')
+      {
+        $sql = "SELECT * FROM status WHERE status_id = $status_id";
+      }
+      else
+      {
+        $sql = "SELECT * FROM status";
+      }
+
+      */
+      // Se asigna al atributo de "query" de la clase "model", donde se aloja la consulta a ejecutar.
+      $this->query = $sql;
+      
+      // Se ejecuta un método de la clase "model" para la ejecución la consulta y se obtienen los datos en un arreglo.
+      $this->get_query;
+
+
     }
     public function update()
     {
